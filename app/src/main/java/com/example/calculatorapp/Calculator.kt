032b023b8d39d9a6b5d508702dfcc842bece77e4
Catherine Gichina <catherinegichinaa@gmail.com>
2024-06-18 06:44:12 +0300

    feat:add new text styles for calculator display

---
+++ app/src/main/java/com/example/calculatorapp/Calculator.kt
@@ -0,0 +1,28 @@
+package com.example.calculatorapp
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Calculator(modifier: Modifier = Modifier) {
+    Box(modifier = Modifier){
+        Column(modifier =  Modifier.fillMaxSize(),horizontalAlignment = Alignment.End) {
+            Text(text = "123 + 123",style = TextStyle(fontSize = 30.sp, textAlign = TextAlign.End), maxLines = 8,overflow = TextOverflow.Ellipsis)
+            Text(text = "246",style = TextStyle(fontSize = 30.sp, textAlign = TextAlign.End), maxLines = 8,overflow = TextOverflow.Ellipsis)
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+    }
+
+}
