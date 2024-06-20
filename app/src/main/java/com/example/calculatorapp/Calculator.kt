6d7c2b8adc822d3a0a103f83abfc2a393c6cabf1
Catherine Gichina <catherinegichinaa@gmail.com>
2024-06-20 23:58:23 +0300

    Feat : add calculator buttons and syle content

--- app/src/main/java/com/example/calculatorapp/Calculator.kt
+++ app/src/main/java/com/example/calculatorapp/Calculator.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,7 +21,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-val myList = listOf(
+val buttonList = listOf(
     "C","()","%","/",
     "7","8","9","*",
     "4","5","6","-",
@@ -33,9 +37,21 @@ fun Calculator(modifier: Modifier = Modifier) {
             Spacer(modifier = Modifier.height(10.dp))
             
             LazyVerticalGrid(columns = GridCells.Fixed(4) ) {
-
+                items(buttonList){
+                 CalculatorButton(btn = it)
+                }
             }
         }
     }
 
+}
+
+@Composable
+fun CalculatorButton(btn : String) {
+    Text(text = btn)
+    Box(modifier = Modifier.padding(8.dp)) {
+       FloatingActionButton(onClick = { }) {
+           Text(text = btn)
+       }
+    }
 }
